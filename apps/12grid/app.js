41601f7ca4926d1fdd15a8cdc6ddf68e2d4826ee
Morgan Wable <morganwablekeene@gmail.com>
2022-07-06 23:25:47 -0400

    rev1_5 - 12grid demo add function
    - Actually called a function within 12grid demo.
    
    - Incremented internal changelog to herald this glorious addition.
    
    notes:
    - this is tedious.
    - See if it *needs* a timeout or an interval. Hopefully not.

--- apps/12grid/app.js
+++ apps/12grid/app.js
@@ -6,6 +6,10 @@ var img = {
   width : 176, height : 176, bpp : 1,
 buffer : require("heatshrink").decompress(atob("AH4AXh//ACXwhEAsEBwEGgEYAxkQCv4V/Cv4Vxh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwCqpt/Cv4V/Cv4VHh//ACXwgAA/ACwA="))
 };
+function draw() {
+  //drawImage
+  g.drawImage(img, 0, 0);
+  Bangle.buzz(100, 0.5);
+}
 
-//drawImage
-g.drawImage(img, 0, 0);
+draw();
